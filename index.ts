0372f83ed8d565f27ed173525024dd90d5c53108
Juniper Bender <jayjayb100@msn.com>
2023-11-03 15:08:33 -0500

    first commit

---
+++ index.ts
@@ -0,0 +1,106 @@
+import {AppBskyFeedPost, AtpSessionData, AtpSessionEvent, BskyAgent, RichText} from "@atproto/api";
+import {ComAtprotoSyncSubscribeRepos, subscribeRepos, SubscribeReposMessage,} from 'atproto-firehose'
+import {RepoOp} from "@atproto/api/dist/client/types/com/atproto/sync/subscribeRepos";
+
+
+let savedSessionData;
+const BSKY_HANDLE: string = <string>Bun.env.BSKY_HANDLE
+const BSKY_PASSWORD: string = <string>Bun.env.BSKY_PASSWORD
+let BOT_DID;
+
+const REPLIES = [
+    'nobody asked',
+    'no one cares',
+    'who cares',
+    'you\'re wrong',
+    'delete ur account',
+    'stfu',
+    'go touch grass',
+    'no one knows what you\'re talking about',
+    'you need to stop'
+]
+
+const agent = new BskyAgent({
+    service: 'https://bsky.social/',
+    persistSession: (evt: AtpSessionEvent, sess?: AtpSessionData) => {
+        BOT_DID = sess?.did
+        savedSessionData = sess;
+    },
+})
+const firehoseClient = subscribeRepos(`wss://bsky.social`, { decodeRepoOps: true })
+
+async function initialize(){
+    await agent.login({identifier: BSKY_HANDLE, password: BSKY_PASSWORD})
+    await agent.resumeSession(savedSessionData)
+}
+
+async function payloadTrigger(op, repo) {
+    const flatText = op.payload.text.toLowerCase()
+
+    let startsWith = flatText.startsWith('well actually') || flatText.startsWith('well, actually');
+    if(!startsWith){
+        return false;
+    }
+    let postDetails = await findPostDetails(op, repo);
+    let split = postDetails.uri.split('/')
+    let postDid = split[2];
+    let postedByBot = postDid === BOT_DID;
+    return startsWith && !postedByBot;
+}
+
+async function findPostDetails(op: RepoOp, repo){
+    let rkey = op.path.split('/')[1]
+    return await agent.getPost({
+        repo: repo, rkey: rkey
+    });
+}
+
+async function handlePayload(op: RepoOp, repo){
+
+    let payload = op.payload;
+
+    let currentPost = await findPostDetails(op, repo);
+
+    const replyText = new RichText({
+        text: `Well actually ${REPLIES[Math.floor(Math.random() * (REPLIES.length - 1))]}`,
+    })
+    let replyPost = await agent.post({
+        reply: {
+            root: payload.reply.root,
+            parent: {
+                cid: currentPost.cid,
+                uri: currentPost.uri
+            }
+        },
+        text: replyText.text
+    })
+    return replyPost;
+}
+
+await initialize();
+
+firehoseClient.on('message', (m: SubscribeReposMessage) => {
+    if (ComAtprotoSyncSubscribeRepos.isCommit(m)) {
+        m.ops.forEach((op) => {
+            let payload = op.payload;
+            switch (payload?.$type){
+                case 'app.bsky.feed.post':
+                    if(AppBskyFeedPost.isRecord(payload)){
+                        if(payload.reply){
+                            payloadTrigger(op, m.repo).then(async (resp) => {
+                                if (resp) {
+                                    console.log('will respond')
+                                    let handle = await handlePayload(op, m.repo)
+                                    console.log(handle)
+                                }
+                            })
+                        }
+                    }
+            }
+        })
+    }
+})
+
+
+
+
